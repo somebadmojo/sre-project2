7924f8b2e962e6f95b7b8a4b3306c96e20beee29
somebadmojo <99145188+somebadmojo@users.noreply.github.com>
2022-05-24 22:18:39 -0700

    Update eks.tf

--- project/starter-code/zone1/eks.tf
+++ project/starter-code/zone1/eks.tf
@@ -21,8 +21,8 @@ provider "kubernetes" {
    ec2_sg             = module.project_ec2.ec2_sg
    vpc_id             = module.vpc.vpc_id
    nodes_desired_size = 2
-   nodes_max_size     = 3
-   nodes_min_size     = 2
+   nodes_max_size     = 2
+   nodes_min_size     = 1
 
    depends_on = [
     module.vpc,
